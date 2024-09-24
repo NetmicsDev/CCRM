f57a13980cf3deb92ca84fa7518dc69a62df8752
sangil <fhemflrh60@gmail.com>
2024-09-24 17:27:16 +0900

    feat: 프로그램 상단 바 ui

--- app/(routes)/(main)/(auth)/sign-in/page.tsx
+++ app/(routes)/(main)/(auth)/sign-in/page.tsx
@@ -1,9 +1,4 @@
-import { Button } from "@/app/_components/Button";
-import { CheckBox } from "@/app/_components/CheckBox";
 import Icon from "@/app/_components/Icon";
-import { Input } from "@/app/_components/Text";
-import cn from "@utils/cn";
-import Image from "next/image";
 import Link from "next/link";
 import LoginForm from "./_components/form";
 import GoogleSignIn from "./_components/google";
@@ -11,7 +6,10 @@ import GoogleSignIn from "./_components/google";
 export default function SignInPage() {
   return (
     <main className="flex flex-col w-[400px] mt-20 m-auto items-center">
-      <Icon type="logo" className="w-48 fill-main-1" />
+      <Icon
+        type="logo"
+        className="w-48 fill-main-1"
+      />
       <LoginForm />
       <div className="w-full mt-4">
         <GoogleSignIn />
